6f26ae41abfe9a40606bcf10d27fb5b6d3f58c40
anders iversen <ande397c@stud.kea.dk>
2022-03-26 12:42:23 +0100

    frontpage strucur and content

--- script/index.js
+++ script/index.js
@@ -7,6 +7,8 @@ function setup() {
 
   arrow.addEventListener("mouseover", changeAnimationSpeed);
   arrow.addEventListener("mouseout", changeAnimationSpeedBack);
+
+  window.addEventListener("scroll", scrolling);
 }
 
 function changeAnimationSpeed() {
@@ -14,4 +16,19 @@ function changeAnimationSpeed() {
 }
 function changeAnimationSpeedBack() {
   document.querySelector(".arrow_splash img").style.animationPlayState = "running";
+}
+
+function scrolling() {
+  const myID = document.querySelector("#customID");
+  let y = window.scrollY;
+
+  if (y >= 700 || y >= 2100) {
+    myID.className = "cta show";
+  } else {
+    myID.className = "cta hide";
+  }
+
+  myID.addEventListener("click", () => {
+    window.scrollTo(0, 0);
+  });
 }
